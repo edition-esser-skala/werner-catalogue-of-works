fd482fa1db5cdf3cbb97d09c94c8e246c604e1bf
Wolfgang Esser-Skala <wolfgang@esser-skala.at>
2024-07-28 12:14:49 +0200

    adds bash script for adding a white background with rounded corners to an SVG image

---
+++ script/svg_add_white_bg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# add a white background to an SVG file, i.e.,
+# add a white-filled <rect> element before the first <defs> tag
+
+sed -i '0,/<defs/s/<defs/<rect width="100%" height="100%" fill="#ffffff" rx="10"\/>\n<defs/' $1
