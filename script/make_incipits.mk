8a1acb1fd2a5aa8497bfd67406f51d02241e770b
Wolfgang Esser-Skala <wolfgang@esser-skala.at>
2024-07-28 12:16:05 +0200

    changes incipit creation: also generate SVGs from LY files

--- script/make_incipits.mk
+++ script/make_incipits.mk
@@ -5,7 +5,6 @@
 LILYPOND = lilypond \
   --include=$(EES_TOOLS_PATH) \
   --include=$(realpath data/incipits) \
-  --png \
   -dno-point-and-click \
   -dcrop
 
@@ -22,17 +21,17 @@ VEROVIO = verovio \
   --scale 33
 
 # all LY files in data/incipits (except header.ly),
-# e.g. data/incipits/Q_2/main.ly -> Q_2/main
+# e.g. data/incipits/Q_2/main_1.ly -> Q_2/main_1
 SOURCES_LY := $(shell find data/incipits -type f -name "*.ly" -and -not -name "header.ly")
 SOURCES_LY := $(SOURCES_LY:data/incipits/%.ly=%)
 
 # all MEI files in data/incipits,
-# e.g. data/incipits/B_46/main.mei -> B_46/main
+# e.g. data/incipits/B_46/main_1.mei -> B_46/main_1
 SOURCES_MEI := $(shell find data/incipits -type f -name "*.mei")
 SOURCES_MEI := $(SOURCES_MEI:data/incipits/%.mei=%)
 
 # all PAE files in data_generated/rism_incipits_pae,
-# e.g. data_generated/rism_incipits_pae/A_5/main.pae -> A_5/main
+# e.g. data_generated/rism_incipits_pae/A_5/main_1.pae -> A_5/main_1
 SOURCES_RISM := $(shell find data_generated/rism_incipits_pae -type f -name "*.pae")
 SOURCES_RISM := $(SOURCES_RISM:data_generated/rism_incipits_pae/%.pae=%)
 
@@ -41,19 +40,15 @@ SOURCES_RISM := $(filter-out $(SOURCES_LY),$(SOURCES_RISM))
 SOURCES_RISM := $(filter-out $(SOURCES_MEI),$(SOURCES_RISM))
 
 # names of manually curated incipits to be created by LilyPond,
-# e.g. incipits/Q_2/main.png
-TARGETS_LY := $(SOURCES_LY:%=incipits/%.png)
-
-# names of corresponding low-resolution PNG files,
-# e.g. incipits/Q_2/main_low.png
-TARGETS_LY_LOW := $(TARGETS_LY:.png=_low.png)
+# e.g. incipits/Q_2/main_1.svg
+TARGETS_LY := $(SOURCES_LY:%=incipits/%.svg)
 
 # names of manually curated incipits to be created by Verovio,
-# e.g. incipits/B_46/main.svg
+# e.g. incipits/B_46/main_1.svg
 TARGETS_MEI := $(SOURCES_MEI:%=incipits/%.svg)
 
 # names of RISM incipits to be created by Verovio,
-# e.g. incipits/A_1_3/main.svg
+# e.g. incipits/A_1_3/main_1.svg
 TARGETS_RISM := $(SOURCES_RISM:%=incipits/%.svg)
 
 
@@ -62,28 +57,27 @@ TARGETS_RISM := $(SOURCES_RISM:%=incipits/%.svg)
 .ONESHELL:
 
 # default target: create all incipits
-all: $(TARGETS_LY) $(TARGETS_LY_LOW) $(TARGETS_MEI) $(TARGETS_RISM)
+all: $(TARGETS_LY) $(TARGETS_MEI) $(TARGETS_RISM)
 
-# how to engrave high-res PNGs from LY
-# alternatively, one could create a PDF and convert to SVG with pdftocairo
-$(TARGETS_LY): incipits/%.png: data/incipits/%.ly data/incipits/header.ly
+# how to engrave SVGs from LY
+$(TARGETS_LY): incipits/%.svg: data/incipits/%.ly data/incipits/header.ly
 >mkdir -p $(@D)
->target_name=$(basename $@ .png)
->$(LILYPOND) -dresolution=300 -o $${target_name} $<
->mv $${target_name}.cropped.png $${target_name}.png
-
-# how to engrave low-res PNGs from LY
-$(TARGETS_LY_LOW): incipits/%_low.png: data/incipits/%.ly data/incipits/header.ly
->target_name=$(basename $@ .png)
->$(LILYPOND) -dresolution=150 -o $${target_name} $<
->mv $${target_name}.cropped.png $${target_name}.png
+>target_name=$(basename $@ .svg)
+>$(LILYPOND) -o $${target_name} $<
+>pdftocairo $${target_name}.cropped.pdf -svg $${target_name}.svg
+>script/svg_scale.sh $${target_name}.svg
+>script/svg_add_white_bg.sh $${target_name}.svg
+>svgo $${target_name}.svg
+>rm $${target_name}.cropped.* $${target_name}.pdf
 
 # how to engrave SVGs from MEI
 $(TARGETS_MEI): incipits/%.svg: data/incipits/%.mei
 >mkdir -p $(@D)
 >$(VEROVIO) -o $@ $<
+>script/svg_add_white_bg.sh $@
 
 # how to engrave SVGs from PAE
 $(TARGETS_RISM): incipits/%.svg: data_generated/rism_incipits_pae/%.pae
 >mkdir -p $(@D)
 >$(VEROVIO) -o $@ $<
+>script/svg_add_white_bg.sh $@
