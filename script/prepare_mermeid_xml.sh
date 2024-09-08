a53491ecb693ac6d431a32fba6ca5751500bf22a
Wolfgang Esser-Skala <wolfgang@esser-skala.at>
2024-09-08 22:02:34 +0200

    XML preparation script: require catalogue of works abbreviation as first argument

--- script/prepare_mermeid_xml.sh
+++ script/prepare_mermeid_xml.sh
@@ -2,7 +2,13 @@
 
 # remove the revision description and rename files
 
-for s in `ls WerW*`; do
+if [ $# -eq 0 ]; then
+  echo "Error: Specify catalogue of works abbreviation as first argument"
+  exit 1
+fi
+
+
+for s in `ls $1*`; do
   t=`echo $s | sed "s/[^_]*_//"`
   echo "Processing $s to $t"
   xmlstarlet edit --inplace --delete "//_:revisionDesc" $s
